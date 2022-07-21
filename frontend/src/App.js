7fd262145102ee69c9b1e4f1eddccb3fa5da4858
chaitu786 <chaithanyareddy786@gmail.com>
2022-07-21 19:38:44 +0530

    some_Modifications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Mainroutes } from "./router/Mainroutes";
+import Otp from "./components/Auth/Otp";
 // import ProductPage from "./components/Pages/Product page/ProductPage"
 
 function App() {
