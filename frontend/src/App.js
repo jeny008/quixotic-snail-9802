f0012d7106a86fcdd51fa3c03f7b357513eed58f
chaitu786 <chaithanyareddy786@gmail.com>
2022-07-22 22:59:06 +0530

    All_Conflicts_resolved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./App.css";
 import { Mainroutes } from "./router/Mainroutes";
-<<<<<<< HEAD
-=======
-// import Otp from "./components/Auth/Otp";
-// import ProductPage from "./components/Pages/Product page/ProductPage"
->>>>>>> 5a31451a8598d28fb793004c72546b7901638135
 
 function App() {
   return (
