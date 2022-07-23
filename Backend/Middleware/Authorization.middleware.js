f1f96266250f7bc1734b64ea978c14b64866bf25
Tapish sharma <tapishsharma1234@gmail.com>
2022-07-23 18:33:07 +0530

    Cookie deletion achieved on logout

--- Backend/Middleware/Authorization.middleware.js
+++ Backend/Middleware/Authorization.middleware.js
@@ -4,6 +4,7 @@ const authenticate = async (mobile) => {
   try {
     const user = await UserModel.find({ mobile });
     if (user) {
+      request.session.cookie.maxAge = 3600000
       return { user };
     }
   } catch (err) {
